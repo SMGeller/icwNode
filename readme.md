1c5f7f1b2c4be2952af4452c7c63a38c7a5747ea
allenwhitedev <allenwhitedev@gmail.com>
2018-03-19 22:24:10 -0400

    Add production/development config, prestart script (installs/updates node_modules), mongo initialization (open a mongo server if mongo daemon is not already running), /api/v1/test route, routes to test mongodb interaction (GET /api/v1/tests and POST /api/v1/tests), readme, and .config.js(ignored by .gitignore since it contains sensitive data)

---
+++ readme.md
@@ -0,0 +1,21 @@
+This is the React app for the IT/CS Workshop at University of Florida. 
+https://icw.cise.ufl.edu
+
+Start the app in development with "npm start".
+The "npm start" command will install all dependencies, start a mongo server if one is not already running, and start the app if the local machine has valid versions of npm, node, and mongodb installed. 
+(Passing with mongo v3.4, node v7.2.0, mongo v3.4.0 -> Build tests will be added in later versions.) 
+
+
+## Production
+Running this app in production version of this app requires the inclusion of .config.js file in the root directory. This file holds config variables that are sensitive and is therefore ignored by .gitignore. 
+An example .config.js is as follows (includes all config variables):
+
+```javascript
+// export config variables
+exports.isConfigEnabled = true // false enables default development configuration with CORS-enabled, true disables CORS and uses config variables defined in this file  
+exports.environment = 'production' // production/development/test
+exports.port = 8080
+exports.saltRounds = 14 // cost factor of bcrypt hashing
+exports.databaseName = 'nameOfMyDatabase' // used by MongoClient and in mongoUrl
+exports.mongoUrl = `mongodb://localhost:27017/${databaseName}` // contains database user info (this is sensitive data!)
+```
--- server.js
+++ server.js
@@ -1,20 +1,111 @@
+// middleware initialization
 const express = require('express')
-const app = express()
+const app = express() 
+app.use( express.json() ) // allow json-encoded bodies in requests (typically for POST/PATCH/DELETE etc.)  
+app.use( express.urlencoded({extended: true}) ) // allow url-encoded bodies in requests, {extended: true} allows nested objects while {extended: false} allows only string or array values in the req.body's key-value pairs
 
-// these will depend on config initialization in the future
-const cors = require('cors')
-app.use( cors() )
-let port = 8000
-let environment = 'development'
+// development/production/test config initialization
+let config = null
+try { config = require('./.config.js') } // load config with sensitive data ()
+catch(e) {
+	if ( e instanceof Error && e.code === 'MODULE_NOT_FOUND' )
+		console.log('Could not find a .config.js in root directory, using a default development config')
+}
+let port, environment, mongoUrl, saltRounds, databaseName // config variables
+if ( config && config.isConfigEnabled ) // setup config from .config.js, cors is disabled in non-default config
+{
+	port = config.port
+	environment = config.environment
+	databaseName = config.databaseName
+	mongoUrl = config.mongoUrl
+	saltRounds = config.saltRounds
+}
+else // use default development config
+{
+	const cors = require('cors')
+	app.use( cors() ) // allow cors in default development environment
+	port = 8000
+	environment = 'development'
+	databaseName = 'icwDevelopment'
+	mongoUrl = `mongodb://localhost:27017/${databaseName}`
+	saltRounds = 12
+}
 
-app.listen(port, () => console.log(`Node app for icw listening on port ${port} in ${environment}`))
+// mongodb initialization 
+const MongoClient = require('mongodb').MongoClient
+const mongod = require('mongod') // mongo daemon
+const mongoServer = new mongod(27017)
+mongoServer.open( err =>
+{
+	if (err)
+		console.log('Error opening mongo server (mongo daemon could already be running):', err)
+	else
+		console.log('Successfully opened mongo server')
+		
+		console.log('Connecting to mongodb...')
+		connectToMongoDb() // assume mongod is already running if mongo server cannot be opening and connect to mongodb regardless of mongoServer.open() result
+})
+let globalDatabase = null // global variable allowing express routes to interact with database 
+function connectToMongoDb()
+{
+	MongoClient.connect(mongoUrl, (err, databases) =>
+	{
+		// mongo 3.4 actually returns databases object containing dbs (including the database name used in mongoUrl)  
+		if ( err )
+			throw err // should implement a way to catch this in future 
+		else
+		{
+			globalDatabase = databases.db(databaseName)
+			console.log(`Success! Connected to '${mongoUrl}'`)
 
+			app.listen(port, () => console.log(`Node app for icw listening on port ${port} in ${environment}`)) // successfully connected to mongodb, start node app
+		}
+	})
+}
+
+// will actually log error to file in future versions
+function logError(error, res) 
+{ 
+	console.log(error) 
+	res.status(500).send({error: true, message: 'Error: Something went wrong with the database or server. The error has been logged.'})
+}
+
+// -- express routes --
 app.get('/', (req, res) =>
 {
 	res.send({message: `Success! This route will serve icw's react app in the future`})
 })
 
+// test routes
 app.get('/test', (req, res) =>
 {
-	res.send({message: `Success! from /test on port ${port} in ${environment}`})
+	res.send({message: `Success! from /test on port ${port} in ${environment}.`})
 })
+app.get('/api/v1/test', (req, res) =>
+{
+	res.send({message: `Success! from version 1 of the api. ( /api/v1/test ) `})
+})
+app.get('/api/v1/tests', (req, res) => // test mongodb - return all documents in 'tests' collection
+{
+	globalDatabase.collection('tests').find( {}, {fields: { _id: 0 } } ).toArray( (err, result) => // return all tests but exclude their _id fields
+	{
+		if ( err )
+			logError(err, res)
+		else
+			res.send( result )
+	} )
+})
+app.post('/api/v1/tests', (req, res) => // test mongodb - add a test to database's 'test' collection
+{
+	console.log('req.body:', req.body)
+	if ( !req.body || !req.body.message )
+		res.status(400).send({error: true, message: `Creating a test requires the following valid fields: 'message'`})
+	else
+		globalDatabase.collection('tests').insert({message: req.body.message}, (err, result) =>
+		{
+			if (err)
+				logError(err, res)
+			else
+				res.status(201).send({message: `Success: Test created with message '${req.body.message}'`})
+		})
+}) 
